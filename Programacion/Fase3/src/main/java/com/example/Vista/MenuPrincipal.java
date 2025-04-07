13e0b4e68d35d4d2020c4e5228e600626723bff9
IbaiLR <ibai.lopez.resta@ikasle.egibide.org>
2025-04-07 10:38:02 +0200

    Menu creado
    Vista1

---
+++ Programacion/Fase3/src/main/java/com/example/Vista/MenuPrincipal.java
@@ -0,0 +1,18 @@
+package com.example.Vista;
+
+import javax.swing.*;
+
+public class MenuPrincipal {
+    private JPanel panel1;
+    private JButton altaDeEquipoButton;
+    private JButton bajaDeEquipoButton;
+    private JButton modificarEquipoButton;
+    private JButton altaDeJugadorButton;
+    private JButton modificarJugadorButton;
+    private JButton generarJornadaButton;
+    private JButton generarEnfrentamientosButton;
+    private JButton TERMINARButton;
+    private JButton bajaDeJugadorButton;
+
+
+}
