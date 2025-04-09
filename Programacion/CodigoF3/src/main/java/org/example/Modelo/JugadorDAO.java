ffc093c6d5432557a120caf22bd226705c244463
Ibai Mendoza <ibai.mendoza@ikasle.egibide.org>
2025-04-09 11:58:18 +0200

    Equipo dao y jugador Dao
    agragar insertar modificar y borrar de las tablas jugadores y equipos

--- Programacion/CodigoF3/src/main/java/org/example/Modelo/JugadorDAO.java
+++ Programacion/CodigoF3/src/main/java/org/example/Modelo/JugadorDAO.java
@@ -3,29 +3,83 @@ package org.example.Modelo;
 import org.example.Controladores.JugadorController;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class JugadorDAO {
-    public static ArrayList<Jugador> listaJugadores;
+    static Connection con = BD.getConnection();
 
-    public JugadorDAO(Connection con) {
-    }
 
-    public static void agregarJugador(Jugador jugador) {
-        listaJugadores.add(jugador);
-
-    }
-    public static void altaJugador() {
-        boolean continuar = false;
-        do {
-            try {
-                Jugador jugador = JugadorController.solicitarValidarDatos();
-                JugadorDAO.agregarJugador(jugador);
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+    public static boolean buscarJugador(String nombreJugador) {
+        //si no es nulo devuelve true
+        //si es nulo devuelve false
+        boolean resultado = false;
+        try {
+            String sql = "SELECT * FROM jugadores WHERE NOMBRE = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, nombreJugador);
+            ps.executeQuery();
+            if (ps != null) {
+                resultado = true;
+            } else {
+                resultado = false;
             }
-        } while (continuar);
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }
+
+    public static void altaJugador(Jugador jugador) {
+        try {
+            String sql = "INSERT INTO jugadores VALUES(?,?,?,?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, jugador.getIdJugador());
+            ps.setString(2, jugador.getNombre());
+            ps.setString(3, jugador.getApellido());
+            ps.setString(4, jugador.getNacionalidad());
+            ps.setDate(5, Date.valueOf(jugador.getFechaNacimiento()));
+            ps.setString(6, jugador.getNickname());
+            ps.setDouble(7, jugador.getSueldo());
+            ps.setString(8, jugador.getEquipo().getIdEquipo());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void modificarJugador(Jugador jugador, String nombreJugador) {
+        try {
+            String sql = "UPDATE JUGADORES SET id_jugador = ?,NOMBRE = ?," +
+                         "apellido = ?,nacionalidad = ?,fecha_nac = ?,nickname = ?,sueldo = ?," +
+                         "id_equipo = ? WHERE NOMBRE= ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, jugador.getIdJugador());
+            ps.setString(2, jugador.getNombre());
+            ps.setString(3, jugador.getApellido());
+            ps.setString(4, jugador.getNacionalidad());
+            ps.setDate(5, Date.valueOf(jugador.getFechaNacimiento()));
+            ps.setString(6, jugador.getNickname());
+            ps.setDouble(7, jugador.getSueldo());
+            ps.setString(8, jugador.getEquipo().getIdEquipo());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void borrarJugador(String nombreJugador) {
+        try {
+            String sql = "DELETE FROM JUGADORES WHERE NOMBRE = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, nombreJugador);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
 }
