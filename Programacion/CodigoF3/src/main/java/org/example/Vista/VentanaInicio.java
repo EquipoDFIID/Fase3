d1e2ea2866b21bed9fab664daf2dc054822e1e60
danelrivas <danel.rivas@ikasle.egibide.org>
2025-04-10 13:58:26 +0200

    Validaciones VentanaInicio
    Validaciones de empty y pattern en Nombre, Clave tanto de Administradores como de Usuarios

--- Programacion/CodigoF3/src/main/java/org/example/Vista/VentanaInicio.java
+++ Programacion/CodigoF3/src/main/java/org/example/Vista/VentanaInicio.java
@@ -5,6 +5,9 @@ import org.example.Controladores.VistaController;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.foreign.MemoryLayout;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VentanaInicio extends JFrame {
     private JPanel panel1;
@@ -15,6 +18,11 @@ public class VentanaInicio extends JFrame {
     private JPanel jUsuario;
     private JRadioButton administradorRadioButton;
     private JRadioButton usuarioRadioButton;
+    private JTextField aNombre;
+    private JTextField aClave;
+    private JTextField uNombre;
+    private JTextField uClave;
+
 
     public VentanaInicio(VistaController vistaController) {
         setContentPane(panel1);
@@ -26,11 +34,31 @@ public class VentanaInicio extends JFrame {
         jAdmin.setVisible(false);
         jUsuario.setVisible(false);
 
-
         administradorRadioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 jAdmin.setVisible(true);
+
+                if (aNombre.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "El nombre no puede estar vacio");
+                } else {
+                    Pattern p = Pattern.compile("^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]*$");
+                    Matcher m = p.matcher(aNombre.getText());
+                    if (!m.matches()) {
+                        JOptionPane.showMessageDialog(null, "El nombre debe comenzar por una mayúscula");
+                    }
+
+                }
+
+                if (aClave.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "La clave no puede estar vacio");
+                } else {
+                    Pattern p = Pattern.compile("^[0-9]{4}$");
+                    Matcher m = p.matcher(aClave.getText());
+                    if (!m.matches()) {
+                        JOptionPane.showMessageDialog(null, "La clave no puede tener ese formato");
+                    }
+                }
             }
         });
 
@@ -38,6 +66,27 @@ public class VentanaInicio extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 jUsuario.setVisible(true);
+
+                if (uNombre.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "El nombre no puede estar vacio");
+                } else {
+                    Pattern p = Pattern.compile("^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]*$");
+                    Matcher m = p.matcher(uNombre.getText());
+                    if (!m.matches()) {
+                        JOptionPane.showMessageDialog(null, "El nombre debe comenzar por una mayúscula");
+                    }
+
+                }
+
+                if (uClave.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "La clave no puede estar vacio");
+                } else {
+                    Pattern p = Pattern.compile("^[0-9]{4}$");
+                    Matcher m = p.matcher(uClave.getText());
+                    if (!m.matches()) {
+                        JOptionPane.showMessageDialog(null, "La clave no puede tener ese formato");
+                    }
+                }
             }
         });
     }
