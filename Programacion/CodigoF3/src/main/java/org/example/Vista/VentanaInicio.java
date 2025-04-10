340c0a34d8b87ccf852bd11f623a79ff08583984
danelrivas <danel.rivas@ikasle.egibide.org>
2025-04-10 13:32:10 +0200

    Ventana de Crear Cuenta y modificación Iniciar Sesión en VentanaInicio

--- Programacion/CodigoF3/src/main/java/org/example/Vista/VentanaInicio.java
+++ Programacion/CodigoF3/src/main/java/org/example/Vista/VentanaInicio.java
@@ -3,13 +3,18 @@ package org.example.Vista;
 import org.example.Controladores.VistaController;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaInicio extends JFrame {
     private JPanel panel1;
     private JButton button1;
     private JButton button2;
-    private JButton usuarioButton;
-    private JButton administradorButton;
+    private JButton crearCuentaButton;
+    private JPanel jAdmin;
+    private JPanel jUsuario;
+    private JRadioButton administradorRadioButton;
+    private JRadioButton usuarioRadioButton;
 
     public VentanaInicio(VistaController vistaController) {
         setContentPane(panel1);
@@ -17,5 +22,23 @@ public class VentanaInicio extends JFrame {
         setTitle("VentanaInicio");
         setSize(500,500);
         setLocationRelativeTo(null);
+
+        jAdmin.setVisible(false);
+        jUsuario.setVisible(false);
+
+
+        administradorRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jAdmin.setVisible(true);
+            }
+        });
+
+        usuarioRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jUsuario.setVisible(true);
+            }
+        });
     }
 }
