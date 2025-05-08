7249835f880ef44eb1e2dc7fc992c40f32616420
danelrivas <danel.rivas@ikasle.egibide.org>
2025-05-08 09:39:30 +0200

    Diseño VentanaJugador

--- Programacion/CodigoF3/src/main/java/org/example/Vista/VentanaJugador.java
+++ Programacion/CodigoF3/src/main/java/org/example/Vista/VentanaJugador.java
@@ -1,48 +1,70 @@
 package org.example.Vista;
 
 import org.example.Controladores.VistaController;
-import org.example.Modelo.EquipoDAO;
 import org.example.Modelo.JugadorDAO;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 public class VentanaJugador extends JFrame {
     private JPanel pPrincipal;
-    private JButton button1;
-    private JTextArea textArea1;
-    private JButton SALIRButton;
-    private JComboBox comboBox1;
+    private JTextArea tJugadores;
+    private JButton bLogo;
+    private JButton bSalir;
+    private JComboBox cEquipos;
     private VistaController vc;
-    private JFrame ventanaUser;
+    private JFrame ventanaUsuario;
+    private String nombre;
 
-    public VentanaJugador(VistaController vc, String nombre, JFrame ventanaUsuario) {
+    public VentanaJugador(VistaController vc, String uNombre, JFrame ventanaUser) {
         this.vc = vc;
-        this.ventanaUser = ventanaUsuario;
+        this.nombre = uNombre;
+        this.ventanaUsuario = ventanaUser;
         setContentPane(pPrincipal);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("MenuPrincipal");
-        setSize(680,580);
+        setSize(500, 580);
         setLocationRelativeTo(null);
-        setResizable(true);
+        setResizable(false);
 
         ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("icon.png"));
         setIconImage(icon.getImage());
-        vc.llenarComboBoxE(comboBox1);
 
-        textArea1.setFont(new Font("Monospaced", Font.PLAIN, 12));
-        comboBox1.addItemListener(new ItemListener() {
+        tJugadores.setEditable(false);
+        vc.llenarComboBoxE(cEquipos);
+
+        tJugadores.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        cEquipos.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 if (e.getStateChange() == ItemEvent.SELECTED) {
-                    String selectedItem = (String) comboBox1.getSelectedItem();
+                    String selectedItem = (String) cEquipos.getSelectedItem();
                     if (!selectedItem.equals("Selecciona un equipo...")) {
-                        textArea1.setText(JugadorDAO.obtenerJugadoresPorEquipo(comboBox1.getSelectedItem().toString()));
+                        tJugadores.setEditable(true);
+                        tJugadores.setText(JugadorDAO.obtenerJugadoresPorEquipo(cEquipos.getSelectedItem().toString()));
+                    } else {
+                        tJugadores.setEditable(false);
+                        tJugadores.setText("");
                     }
                 }
 
+            }
+        });
+
+        bSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ventanaUser.setVisible(true); // Vuelve a mostrar la ventana de administrador
+                dispose(); //
+            }
+        });
+        bLogo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                vc.mostrarVentanaInicio();
             }
         });
     }
